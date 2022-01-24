61cb350b8f544e1f20466c95737a5775b684ebe5
Priecn <prince.silverarrow@gmail.com>
2022-01-24 18:16:52 +0530

    Initial commit

---
+++ src/main/java/learn/jms/jmsbasics/JmsBasicsApplication.java
@@ -0,0 +1,13 @@
+package learn.jms.jmsbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/jms/jmsbasics/JmsBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.jms.jmsbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
